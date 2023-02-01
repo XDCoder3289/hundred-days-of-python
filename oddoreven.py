39a8e7453982906f0f4aa612267e8e2a8b5b2e56
Graana <graana@Graanas-MacBook-Pro-2.local>
2023-02-01 17:26:40 +0500

    adding odd or even code

---
+++ oddoreven.py
@@ -0,0 +1,10 @@
+# odd or even code
+
+num = int(input("What's the number: "))
+
+if num % 2 == 0:
+    print("The number is even")
+else:
+    print("The number is odd")
+
+# Modulo gives the remainder
