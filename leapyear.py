25697d0106a0179b3792a9f1bb46bcdbe82e8d07
Graana <graana@Graanas-MacBook-Pro-2.local>
2023-02-02 14:24:52 +0500

    adding leapyear.py

---
+++ leapyear.py
@@ -0,0 +1,14 @@
+# A program to check if a year is a leap year
+
+year = int(input("Please enter the year you want to check: "))
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print(f"{year} is a leap year")
+        else:
+            print(f"{year} is NOT a leap year")
+    else:
+        print(f"{year} is a leap year")
+else:
+    print(f"{year} is NOT a leap year")
