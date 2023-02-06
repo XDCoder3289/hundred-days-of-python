e7b0ed3fb28cea7a57e345d142f3b405be865395
Graana <xdcoder3289@gmail.com>
2023-02-06 23:40:52 +0500

    greet.py

---
+++ greet.py
@@ -0,0 +1,17 @@
+# def greet():
+#     print("Hello")
+#     print("My name is Salman")
+#     print("How are you doing")
+
+# greet()
+
+# def greet_with_name(name):
+#     print(f"Hello {name}")
+
+# greet_with_name("Sl")
+
+def greet_with_location(name, location):
+    print(f"hello {name}")
+    print(f"what is it like in {location}")
+
+greet_with_location("Salman", "Islamabad")
