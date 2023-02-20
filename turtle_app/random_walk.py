67e4a99c24fd42c2c708bafad5bb4ca5ce253aa1
Salman <xdcoder3289@gmail.com>
2023-02-21 01:55:43 +0500

    changes

--- turtle_app/random_walk.py
+++ turtle_app/random_walk.py
@@ -1,17 +1,30 @@
 from turtle import Turtle, Screen
 import random
 
-
 mish = Turtle()
-rando = random.randint(0, 10)
+mish.pensize(10)
+mish.speed(6)
 
+color = ["blue", "green", "pink", "seagreen"]
+
+c = 0
 i = 0
-while i < 10:
-    mish.forward(rando.rando())
-    mish.left(rando.rando())
-    mish.right(rando.rando())
-    mish.backward(rando.rando())
+
+while i < 50:
+    if c > 3:
+        c = 0
+    mish.color(color[c])
+    rando1 = random.randint(10, 50)
+    rando2 = random.randint(50, 100)
+    rando3 = random.randint(10, 100)
+    rando4 = random.randint(50, 100)
+    mish.forward(rando1)
+    mish.right(90)
+    mish.forward(rando1)
+    mish.backward(rando3)
+    mish.left(90)
     i += 1
+    c += 1
 
 screen = Screen()
 screen.exitonclick()
